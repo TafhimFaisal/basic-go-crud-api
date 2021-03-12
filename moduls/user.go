ee2febe8358c387a57fe131eb69c170876ac6b6f
TafhimFaisal <tropical10tafhim@gmail.com>
2021-03-12 16:49:21 +0600

    removing user

--- moduls/user.go
+++ moduls/user.go
@@ -54,3 +54,14 @@ func UpdateUser(u User) (User, error) {
 
 	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
 }
+
+func RemoveUserbyId(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("user with ID '%v' not found", id)
+}
