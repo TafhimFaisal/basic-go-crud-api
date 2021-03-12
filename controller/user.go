ae5876f8d5bc50174447de955a6255c7546d6691
TafhimFaisal <tropical10tafhim@gmail.com>
2021-03-12 21:46:54 +0600

    controller

--- controller/user.go
+++ controller/user.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 	"regexp"
+
+	"github.com/TafhimFaisal/basic_crud_with_golang/moduls"
 )
 
 // UserController to controll user
@@ -12,6 +15,82 @@ type UserController struct {
 
 func (uc UserController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello from user controller !!!"))
+}
+
+func (uc *UserController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(moduls.GetUsers(), w)
+}
+
+func (uc *UserController) get(id int, w http.ResponseWriter) {
+	u, err := moduls.GetUserByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) post(w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	u, err = moduls.AddUser(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) put(id int, w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not pars user object"))
+		return
+	}
+
+	if id != u.ID {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	u, err = moduls.UpdateUser(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) delete(id int, w http.ResponseWriter) {
+	err := moduls.RemoveUserbyId(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func (uc *UserController) parseRequest(r *http.Request) (moduls.User, error) {
+	dec := json.NewDecoder(r.Body)
+	var u moduls.User
+	err := dec.Decode(&u)
+
+	if err != nil {
+		return moduls.User{}, err
+	}
+
+	return u, nil
 }
 
 func newUserController() *UserController {
